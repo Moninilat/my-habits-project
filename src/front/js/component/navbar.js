87192985aa4341f2b215e88fdec7e9ac1c9587bb
Mangodev <migudz@gmail.com>
2025-01-21 16:33:19 +0000

    estructura

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { MenuListComposition } from "./menu.js";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<MenuListComposition />
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
+
+    const handleResize = () => {
+        setIsMobile(window.innerWidth <= 800);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    return (
+        <nav className="navbar">
+            {isMobile ? (
+                <div className="burger-menu">
+                    <button className="burger-button">
+                        <i className="fas fa-bars"></i>
+                    </button>
+                    <div className="burger-content">
+                        <Link to="/">Inicio</Link>
+                        <Link to="/about">Acerca de</Link>
+                        <Link to="/contact">Contacto</Link>
+                    </div>
+                </div>
+            ) : (
+                <div className="complete-menu">
+                    <Link to="/">Inicio</Link>
+                    <Link to="/about">Acerca de</Link>
+                    <Link to="/contact">Contacto</Link>
+                </div>
+            )}
+        </nav>
+    );
 };
