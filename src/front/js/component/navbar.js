7a490f6e41bbc87ed0e719675854e1ce622fcd31
Mangodev <migudz@gmail.com>
2025-01-21 13:19:32 +0000

    Manuel React update

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { MenuListComposition } from "./menu.js";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
+				<MenuListComposition />
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
