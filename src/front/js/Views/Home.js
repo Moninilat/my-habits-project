aedfdc8f154ce34685c684322f9781109d5034e9
Erik Martin <erikmartinfernandez@gmail.com>
2025-01-23 11:19:17 +0000

    componentes reutilizables nuevos

--- src/front/js/Views/Home.js
+++ src/front/js/Views/Home.js
@@ -1,21 +1,24 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
 import { Diversity1Outlined } from "@mui/icons-material";
 import { Navbar } from "../component/navbar";
-import { Calendar } from "../component/calendar";
+import { Calendar, SmallHabit } from "../component/smallhabit";
 import { HabitCard } from "../component/habitcard";
 import { NewHabitCard } from "../component/newhabitcard";
+import { User } from "../component/user";
+import { UserScore } from "../component/userscore";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div>
-			<Calendar />
+			<SmallHabit />
 			<HabitCard />
 			<NewHabitCard />
+			<User />
+			<UserScore />
 		</div>
 	);
 }
--- src/front/js/component/newhabitcard.js
+++ src/front/js/component/newhabitcard.js
@@ -2,12 +2,10 @@ import React, { Component } from "react";
 import "../../styles/newhabitcard.css"
 
 export const NewHabitCard = () => (
-    <div className="newhabitcard d-flex space-between">
-        <div className="habit">
-            <p>Add habit</p>
-        </div>
-        <div className="habit">
-            <p className="btn">+</p>
+    <div className="new_habit_component">
+        <div className="new_habit_title">Add habit</div>
+        <div className="new_habit_box">
+            <div className="new_habit_button"><i className="fa-solid fa-plus"></i></div>
         </div>
     </div>
 );
