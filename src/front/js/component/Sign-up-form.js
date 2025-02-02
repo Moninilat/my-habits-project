8ad37e54952f90edb1cb1a9cd2ab2298ca9629b4
Mangodev <migudz@gmail.com>
2025-02-02 12:51:51 +0000

    funcionalidad-profile-picture

--- src/front/js/component/Sign-up-form.js
+++ src/front/js/component/Sign-up-form.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from 'react';
 import { Context } from '../store/appContext';
 import "../../styles/login.css";
 import Visibility from '@mui/icons-material/Visibility';
@@ -7,6 +7,7 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormLabel from '@mui/material/FormLabel';
+
 export const SignUp = () => {
   const [data, setData] = useState({
     first_name: "",
@@ -16,27 +17,41 @@ export const SignUp = () => {
     repeatPassword: "",
     city: "",
     gender: ""
+  });
 
-  })
-  const [firstName, setfirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
   const [showPasswordA, setShowPasswordA] = useState(false);
   const [showPasswordB, setShowPasswordB] = useState(false);
-  const { store, actions } = useContext(Context)
-  const navigate = useNavigate()
+  const { store, actions } = useContext(Context);
+  const [userPicture, setUserPicture] = useState(localStorage.getItem("image") || "");
+  const navigate = useNavigate();
+
+  
+
   const handleChange = (e) => {
-    setData({ ...data, [e.target.name]: e.target.value })
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const toggleShowPasswordA = () => setShowPasswordA(!showPasswordA);
+  const toggleShowPasswordB = () => setShowPasswordB(!showPasswordB);
+
+  
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setUserPicture(imageUrl);
+      localStorage.setItem("image", imageUrl);
+      store.user_picture_profile(userPicture)
+
+  };
   }
-  const toggleShowPasswordA = () => {
-    setShowPasswordA(!showPasswordA);
+  const handleDeletePicture = () => {
+    localStorage.removeItem("image");
+    setUserPicture("");
   };
-  const toggleShowPasswordB = () => {
-    setShowPasswordB(!showPasswordB);
-  };
+
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -46,99 +61,106 @@ export const SignUp = () => {
     }
 
     actions.signUp(data, navigate);
-  }
-console.log(data);
+  };
 
   return (
     <div className="signup-box">
       <form id="signup-form" onSubmit={handleSubmit}>
-
         <input
-          type="text"
-          name="first_name"
-          placeholder="Nombre..."
-          value={data.first_name}
-          onChange={handleChange}
-          pattern="[A-Za-z]{3-16}"
-          required
-        />
+          type="text" 
+          name="first_name" 
+          placeholder="Nombre..." 
+          value={data.first_name} 
+          onChange={handleChange} 
+          required />
 
+        <input 
+        type="text" 
+        name="last_name" 
+        placeholder="Apellido..." 
+        value={data.last_name} 
+        onChange={handleChange} 
+        required />
 
-        <input
-          type="text"
-          name="last_name"
-          placeholder="Apellido..."
-          value={data.last_name}
-          onChange={handleChange}
-          pattern="[A-Za-z]{3-16}"
-          required
-        />
-
-
-        <input
-          type="text"
-          name="email"
-          placeholder="Correo electrónico..."
-          value={data.email}
-          onChange={handleChange}
-          pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
-          required
+        <input 
+        type="email" 
+        name="email" 
+        placeholder="Correo electrónico..." 
+        value={data.email} 
+        onChange={handleChange} 
+        required 
         />
 
         <div className='password-box'>
-
-          <input
-            type={showPasswordA ? "text" : "password"}
-            name="password"
-            placeholder="Contraseña..."
-            value={data.password}
-            onChange={handleChange}
-            pattern="[A-Za-z][0-9]{3-16}"
-            required
+          <input 
+          type={showPasswordA ? "text" : "password"} 
+          name="password" placeholder="Contraseña..." 
+          value={data.password} onChange={handleChange} 
+          required 
           />
           <Visibility className='visibility' onClick={toggleShowPasswordA} />
         </div>
-        <div className='password-box'>
 
-          <input
-            type={showPasswordB ? "text" : "password"}
-            name="repeatPassword"
-            placeholder="Confirmar contraseña..."
-            value={data.repeatPassword}
-            onChange={handleChange}
-            required
-          >
-          </input>
+        <div className='password-box'>
+          <input 
+          type={showPasswordB ? "text" : "password"} 
+          name="repeatPassword" 
+          placeholder="Confirmar contraseña..." 
+          value={data.repeatPassword} 
+          onChange={handleChange} 
+          required />
           <Visibility className="visibility" onClick={toggleShowPasswordB} />
         </div>
 
-        <input
-          type="text"
-          name="city"
-          placeholder="Ciudad..."
-          value={data.city}
-          onChange={handleChange}
-          pattern="[A-Za-z]{3-16}"
-          required
+        <input type="text" 
+        name="city" 
+        placeholder="Ciudad..." 
+        value={data.city} 
+        onChange={handleChange} 
+        required 
         />
+
         <div>
-          <FormLabel id="demo-radio-buttons-group-label">Género</FormLabel>
-          <RadioGroup
-            onChange={handleChange}
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="female"
-            value={data.gender}
-            name="gender"
-          >
-            <FormControlLabel value="female" control={<Radio />} label="Femenino" />
-            <FormControlLabel value="male" control={<Radio />} label="Masculino" />
+          <FormLabel>Género</FormLabel>
+          <RadioGroup 
+          onChange={handleChange} 
+          value={data.gender} 
+          name="gender">
+
+            <FormControlLabel 
+            value="female" 
+            control={<Radio />} 
+            label="Femenino" />
+            <FormControlLabel 
+            value="male" 
+            control={<Radio />} 
+            label="Masculino" />
 
           </RadioGroup>
         </div>
 
+        {/* Subida de imagen */}
+        <div className='profile-picture'>
+          {userPicture && <img src={userPicture} 
+          alt="Perfil" 
+          style={{ width: "90px", height: "90px", cursor:"pointer" }}
+          onClick={handleDeletePicture}
+        />}
+
+        <div className="file-loader" id='file-loader'>
+          <input 
+          name='file-loader'
+          type="file"
+          className='file-loader' 
+          accept="image/*" 
+          onChange={handleImageUpload} 
+          />
+        </div>
+
+        </div>
+
         <button className="signup-submit-button" type="submit">Registrarse</button>
       </form>
-
     </div>
-  )
-}
+  );
+};
