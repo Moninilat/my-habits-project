5d05458057eb6df381e8d61b46a3443b13987d64
Erik Martin <erikmartinfernandez@gmail.com>
2025-02-03 19:03:36 +0000

    css y cositas

--- src/front/js/Views/Home.js
+++ src/front/js/Views/Home.js
@@ -9,63 +9,64 @@ import { useNavigate } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Home = () => {
-    const { store, actions } = useContext(Context);
-    const navigate = useNavigate()
-    useEffect(() => {
-        if (!localStorage.getItem("token")) {
-            navigate("/")
-        }
-        actions.getHabits();
-        actions.getUser();
-        actions.getUserHabits();
-        actions.getRanking();
-    }, []);
+  const { store, actions } = useContext(Context);
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/")
+    }
+    actions.getHabits();
+    actions.getUser();
+    actions.getUserHabits();
+    actions.getRanking();
+  }, []);
 
-    return (
-      <div className="home">
-            <div className="containerr">
-              {/* Sección Ranking con carrusel */}
-              <section className="carousel-section">
-                <h2>Ranking</h2>
-                <div className="carousel ranking-carousel">
-                  {store.ranking.map((user) => (
-                    <UserScore
-                      key={`${user.id}-${user.name}`}
-                      name={user.first_name}
-                      city={user.city}
-                      score={user.score}
-                    />
-                  ))}
-                </div>
-              </section>
-        
-             
-        
-              {/* Sección Habits*/}
-              <section className="carousel-section">
-        <h2>Recomendaciones</h2>
-        <div className="carousel small-habits-carousel">
-          {store.habits.map((habit, index) => (
-            <SmallHabit key={index} habit={habit} />
-          ))}
-        </div>
-      </section>
+  return (
+    <div className="home">
+      <div className="containerr">
+        {/* Sección Ranking con carrusel */}
+        <section className="carousel-section">
+          <h2>Ranking</h2>
+          <div className="carousel ranking-carousel">
+            {store.ranking.map((user) => (
+              <UserScore
+                key={`${user.id}-${user.name}`}
+                name={user.first_name}
+                city={user.city}
+                score={user.score}
+              />
+            ))}
+          </div>
+        </section>
 
-               {/* Sección user_habits */}
-               <section className="carousel-section">
-                <h2>Mis Hábitos</h2>
-                <div className="carousel habits-carousel">
-                  {store.user_habits && store.user_habits.length > 0 ? (
-                    store.user_habits.map((user_habit, index) => (
-                      <HabitCard key={index} user_habit={user_habit} />
-                    ))
-                  ) : (
-                    <div className="no-habits">¡Empieza a añadir tus hábitos!</div>
-                  )}
-                </div>
-              </section>
-            </div>
-            </div>
-          );
-        
+
+
+        {/* Sección Habits*/}
+        <section className="carousel-section">
+          <h2>Recomendaciones</h2>
+          <div className="carousel small-habits-carousel">
+            {store.habits.map((habit, index) => (
+              <SmallHabit key={index} habit={habit} />
+            ))}
+          </div>
+        </section>
+
+        {/* Sección user_habits */}
+        <section className="carousel-section">
+          <h2>Mis Hábitos</h2>
+        </section>
+
+        <div class="habit">
+            {store.user_habits && store.user_habits.length > 0 ? (
+              store.user_habits.map((user_habit, index) => (
+                <HabitCard key={index} user_habit={user_habit} />
+              ))
+            ) : (
+              <div className="no-habits">¡Empieza a añadir tus hábitos!</div>
+            )}
+          </div>
+      </div>
+    </div>
+  );
+
 }
